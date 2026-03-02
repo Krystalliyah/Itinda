d5839453fd707e5d5aed53b80517b8959d734e50
unknown <kristinekylle.ninal@g.msuiit.edu.ph>
2026-03-03 00:44:25 +0800

    cleaned code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            RoleSeeder::class,
-        ]);
 
         $this->call(RolesSeeder::class);
+        $this->call(AdminUserSeeder::class);
     }
 }
